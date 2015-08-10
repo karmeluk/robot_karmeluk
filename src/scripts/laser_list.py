7ccfa1c2c5236efb05f62b19d1e6cdbe47dc163e
karmeluk <andrii.kudriashov@gmail.com>
2015-08-10 20:56:34 +0200

    For testing only

--- src/scripts/laser_list.py
+++ src/scripts/laser_list.py
@@ -7,32 +7,26 @@ import math
 
 from numpy import arange
 from sensor_msgs.msg import LaserScan
+from time import sleep
 
 
-def mapofsample(scan):
+def mapofsamples(scan):
     iterator = 0
     mapa = []
     for theta in arange(scan.angle_min, scan.angle_max, scan.angle_increment):
-        # print theta
-        # print itera
-        # print math.degrees(theta)
-
         x = scan.ranges[iterator] * math.cos(math.degrees(theta))
         y = scan.ranges[iterator] * math.sin(math.degrees(theta))
-
         # print "x: %s, y: %s" % (x,y)
         iterator += 1
-        mapa.append({'x': x, 'y': y})
+        mapa.append([x,y])
         if iterator == 1080:
-            return map
+            return mapa
 
 
 def callback(scan):
-    # rospy.loginfo("start: " + str(scan.angle_min))
-    # rospy.loginfo("stop: " + str(scan.angle_max))
-    # rospy.loginfo("increment" + str(scan.header))
     # rospy.loginfo("ranges: " + str(scan.ranges))
-    mapa = mapofsample(scan)
+    mapa = mapofsamples(scan)
+    sleep(2)
     print mapa
     # TODO: add bridge with web-server
 
@@ -41,7 +35,6 @@ def listener():
     rospy.init_node('listener_karmeluk')
 
     rospy.Subscriber("most_intense", LaserScan, callback)
-
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
