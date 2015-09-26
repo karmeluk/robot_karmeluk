5be95c1ea9cccd83646c4afd69e26a0c5225c2e4
karmeluk <andrii.kudriashov@gmail.com>
2015-09-26 21:07:44 +0200

    fixed directions, js message queued etc

--- src/scripts/laser_web.py
+++ src/scripts/laser_web.py
@@ -24,12 +24,20 @@ class LaserListener(Thread):
         self.zmq_socket.bind("tcp://*:%s" % self.zmq_port)
 
     def listen(self):
-        mapa = self.zmq_socket.recv()
-        print "received: ", mapa
-        sleep(1)
-        self.zmq_socket.send("got it! port used: %s" % self.zmq_port)
+        # self.zmq_socket.setsockopt(zmq.CONFLATE, 1)
+        for _ in range(100):
+
+            mapa = self.zmq_socket.recv()
+            #print "received: ", mapa
+            self.zmq_socket.send("got it! port used: %s" % self.zmq_port)
+
         return mapa
 
+#
+# msg = zmq.receive()
+# if (self.last_received > 1 sek):
+# self.update(ms)
+# self.last_received == datetime.now()
 
 ASSETS_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../scripts")
 TEMPLATE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../scripts/templates")
@@ -43,15 +51,20 @@ listener.start()
 @app.route('/index', methods=['GET', 'POST'])
 def index(chartID = 'chart_ID'):
     mapa = json.loads(listener.listen())
+    for elem in mapa:
+        elem[:] = [x*100 for x in elem]
+
     # chart = {"renderTo": chartID}
-    title = {"text": 'Robot Karmeluk'}
-    subtitle = {"text": 'Laser Map'}
-    xAxis = {"gridLineWidth": "1", "title": {"enabled": "true", "text": 'X ranges (cm)'},\
+    title = {"text": 'Robot Scanner'}
+    subtitle = {"text": 'Lidar Map'}
+    xAxis = {"gridLineWidth": "1", "title": {"enabled": "true", "text": 'Y ranges (cm)'},\
              "startOnTick": "true", "endOnTick": "true", "showLastLabel": "true"}
-    yAxis = {"title": {"text": 'Y ranges (cm)'}}
+    yAxis = {"title": {"text": 'X ranges (cm)'}}
     legend = {"layout": 'vertical', "align": 'right', "verticalAlign": 'middle'}
     series = [{"name": 'Observated ranges', "type": 'polygon', "data": mapa,\
-               "color": 'yellow', "enableMouseTracking": 'false'}]
+               "color": 'rgba(131, 255, 94, 0.6)', "enableMouseTracking": 'false'},
+              {"name": 'Robot position', "type": 'scatter', "data": [[0.0, 0.0], [0.0, 0.0]],\
+               "color": 'red', "enableMouseTracking": 'false'}]
     tooltip = {"headerFormat": '<b>{series.name}</b><br>', "pointFormat": 'X:{point.x} cm, Y:{point.y} cm'}
 
     return render_template('index.html', chartID=chartID, title=title, subtitle=subtitle, xAxis=xAxis, yAxis=yAxis,\
